bf1345042008ae7069edae064c665c2a9d0e486a
Mahmoud Nabhan <niypoo@gmail.com>
2025-01-20 13:03:21 +0200

    add comments

--- lib/patata_responsive.dart
+++ lib/patata_responsive.dart
@@ -2,14 +2,20 @@ import 'dart:ui';
 
 extension PatataResponsive on num {
   
-  // handle device sizes
+  /// handle device sizes
   FlutterView get view => PlatformDispatcher.instance.views.first;
+  /// define a get device width
   double get physicalWidth => view.physicalSize.width;
+  /// define a get device height
   double get physicalHeight => view.physicalSize.height;
+  /// define a get device ratio
   double get devicePixelRatio => view.devicePixelRatio;
-  
+
+  /// define a get is landscape mode
   bool get isLandscape => view.physicalSize.aspectRatio > 1;
+  /// define a get screen width
   double get screeWidth => physicalWidth / devicePixelRatio;
+   /// define a get screen height
   double get screenHeight => physicalHeight / devicePixelRatio;
 
 
